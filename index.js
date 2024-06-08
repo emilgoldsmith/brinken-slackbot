bd81834a58680cf93f95e0be28c063df39c8c5ee
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2024-06-08 16:11:42 +0200

    Testing sheet db

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const { App } = require("@slack/bolt");
+const sheetdb = require("sheetdb-node");
 
 // Initializes your app with your bot token and signing secret
-const app = new App({
+const slackApp = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
+const sheetDbClient = sheetdb({
+  address: "jp1hjy317fz23",
+  auth_login: process.env.SHEET_DB_LOGIN,
+  auth_password: process.env.SHEET_DB_PASSWORD,
+});
+
+SHEET_NAME = "Sheet1";
+
 (async () => {
   const port = process.env.PORT || 3000;
   // Start your app
-  await app.start(port);
+  await slackApp.start(port);
 
   console.log("⚡️ Bolt app is running on port " + port + "!");
 
-  await app.client.chat.postMessage({
+  await slackApp.client.chat.postMessage({
     channel: "slackbot-test",
     text: "testing 1, 2, 3",
   });
+
+  const response = await sheetDbClient.create([
+    { id: "INCREMENT", name: "test", time: "TIMESTAMP" },
+  ]);
+  console.log(response);
 })();
