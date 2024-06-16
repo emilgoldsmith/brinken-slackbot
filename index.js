a7fcb04000836190207de9b1b7adc13caaec87a2
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2024-06-16 12:56:13 +0200

    Add in the test slash command

--- index.js
+++ index.js
@@ -46,6 +46,73 @@ const slackApp = new App({
   ],
 });
 
+slackApp.command("/brinkenbot-test", async ({ command, ack, respond }) => {
+  await ack();
+  const params = command.text
+    .split(" ")
+    .map((x) => x.trim())
+    .filter((x) => x);
+  if (params.length <= 0) {
+    await respond({
+      text: `Her er dine muligheder for tests du kan bede om:
+
+1. \`/brinkenbot-test føds uge [MM-DD]\` så for eksempel \`/brinkenbot-test føds uge 05-26\`
+2. \`/brinkenbot-test føds dag [kanal-navn]`,
+    });
+    return;
+  }
+
+  const testName = params[0];
+  switch (testName) {
+    case "føds": {
+      if (params.length < 2) {
+        await respond({
+          text: "Inkorrekt føds kommando, der skal være en underkommando der enten er `uge` eller `dag`",
+        });
+        return;
+      }
+      const subCommand = params[1];
+      if (subCommand === "uge") {
+        if (params.length !== 3) {
+          await respond({
+            text: "Inkorrekt føds uge kommando, forventede `/brinkenbot-test føds uge [MM-DD]` format",
+          });
+          return;
+        }
+        const targetBirthdayMMDD = params[2];
+        await handleWeekBeforeBirthday(
+          targetBirthdayMMDD,
+          crypto.randomBytes(5).toString("hex")
+        );
+      } else if (subCommand === "dag") {
+        if (params.length !== 3) {
+          await respond({
+            text: "Inkorrekt føds dag kommando, forventede `/brinkenbot-test føds dag [kanal-navn]` format",
+          });
+          return;
+        }
+        const channelName = params[2];
+        await handleDayBeforeBirthday("", channelName);
+      } else {
+        await respond({
+          text:
+            "Inkorrekt føds underkommando " +
+            subCommand +
+            ", eneste muligheder er `uge` eller `dag`",
+        });
+        return;
+      }
+      break;
+    }
+
+    default: {
+      await respond({
+        text: `Uventet test ${testName}, eneste understøttede mulighed p.t. er \`føds\``,
+      });
+    }
+  }
+});
+
 (async () => {
   const port = process.env.PORT || 3000;
   // Start your app
@@ -164,15 +231,20 @@ async function handleWeekBeforeBirthday(
 
 async function handleDayBeforeBirthday(
   targetBirthdayMMDD,
-  channelNameSuffix = ""
+  channelNameOverride = null
 ) {
-  const { birthdayPeople } = await getBirthdayPeople(targetBirthdayMMDD);
-  if (birthdayPeople.length <= 0) return;
+  let birthdayChannelName;
+  if (channelNameOverride !== null) {
+    birthdayChannelName = channelNameOverride;
+  } else {
+    const { birthdayPeople } = await getBirthdayPeople(targetBirthdayMMDD);
+    if (birthdayPeople.length <= 0) return;
 
-  const birthdayChannelName = buildBirthdayChannelName(
-    birthdayPeople,
-    channelNameSuffix
-  );
+    birthdayChannelName = buildBirthdayChannelName(
+      birthdayPeople,
+      channelNameSuffix
+    );
+  }
 
   await slackApp.client.chat.postMessage({
     channel: birthdayChannelName,
