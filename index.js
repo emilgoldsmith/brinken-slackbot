8c49055cd29a27d4679af9a3743e8d6512adaedf
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2025-01-05 17:17:28 +0100

    Remove the temp stuff

--- index.js
+++ index.js
@@ -189,12 +189,12 @@ console.log("Discord App Initialized And Running");
 //   .then(console.log)
 //   .then(() => discordClient.destroy())
 //   .then(() => process.exit(0));
-const guild = await discordClient.guilds.fetch(DISCORD_GUILD_ID);
-await guild.channels
-  .fetch()
-  .then(console.log)
-  .then(() => discordClient.destroy())
-  .then(() => process.exit(0));
+// const guild = await discordClient.guilds.fetch(DISCORD_GUILD_ID);
+// await guild.channels
+//   .fetch()
+//   .then(console.log)
+//   .then(() => discordClient.destroy())
+//   .then(() => process.exit(0));
 
 // await handleThreeDaysBeforeDinner(
 //   DateTime.now().set({ weekday: 4 }).plus({ week: 1 })
